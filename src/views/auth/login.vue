f98eaeb88255abe982bb43d543f5578015a1b235
habeeb-kredi <h.amode@thekredibank.com>
2022-07-28 23:47:42 +0100

    administrator feature fixed

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -29,6 +29,7 @@
                       </div>
 
                       <div class="p-2 mt-5">
+                        <b-alert show variant="danger" class="text-center" v-if="this.error">{{this.error}}</b-alert>
                         <form
                           class="form-horizontal"
                           @submit.prevent="login()"
@@ -61,11 +62,19 @@
 
                           <div class="mt-4 text-center">
                             <button
-                              class="btn btn-primary w-md waves-effect waves-light btn-block"
+                              class="btn btn-primary w-md waves-effect waves-light btn-block" v-show="!isLoading"
                               type="submit"
                             >
                               Log In
                             </button>
+
+                            <b-button v-show="isLoading" variant="primary"
+                             class="btn btn-primary btn-block w-md waves-effect waves-light"
+                             disabled
+                             >
+                              <b-spinner small type="grow"></b-spinner>
+                              Loading...
+                            </b-button>
                           </div>
                         </form>
                       </div>
@@ -96,6 +105,8 @@ export default {
         password: "",
       },
       submitted: false,
+      error: "",
+      isLoading: false,
     };
   },
   computed: {},
@@ -106,25 +117,24 @@ export default {
     // Try to log the user in with the username
     // and password they provided.
     login: function () {
-      // this.isLoading = true;
+      this.isLoading = true;
       this.$store
         .dispatch("login", {
           email: this.user.email,
           password: this.user.password,
         })
         .then(() => {
-          // this.isLoading = false;
-          // console.log(response);
           this.$router.push({ name: "dashboard" });
         })
         .catch((err) => {
-          if (err.response.status == 400) {
-            this.error = "Invalid login details";
-            // this.isLoading = false;
+          console.log(err.response);
+          if (err.response.status == 401) {
+            this.error = err.response.data.error;
+            this.isLoading = false;
           }
           if (err.response.status == 422) {
-            this.error = "Fields are empty";
-            // this.isLoading = false;
+            this.error = err.response.data.error;
+            this.isLoading = false;
           }
         });
     },
